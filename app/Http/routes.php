0e6d53e1392cc0a0b483598e3349708eb0162aff
Andrei Martins <amartins13@outlook.com>
2016-07-27 01:19:39 +0000

    Delete task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -50,5 +50,6 @@ Route::post('/task', function (Request $request) {
  * Delete Task
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+    return redirect('/');
 });
