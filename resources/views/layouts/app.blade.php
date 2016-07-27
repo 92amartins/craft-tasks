66550991656323ff2188937a6af5662f262da203
Andrei Martins <amartins13@outlook.com>
2016-07-27 02:09:36 +0000

    Add Bootstrap and Font Awesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,24 @@
         <title>ToDo List</title>
 
         <!-- CSS And JavaScript -->
+        
+        
+        <!-------------- 
+        --- Bootstrap  - 
+        --------------->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        
+        <!----------------- 
+        --- Font Awesome  - 
+        ------------------>
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     </head>
 
     <body>
